eae123bbe79e21aa11db340c95d855ea7a8471f1
Dina Ayoub <dina.ayoub@vodafone.com>
2022-08-31 16:47:34 +0200

    DEVELOP: Add not found page - enhancement

---
+++ src/components/Pages/notFound.tsx
@@ -0,0 +1,13 @@
+import './notFound.css';
+
+function NotFound() {
+    return (
+        <div>
+            <h2 className="notFound">
+                Sorry, page not found
+            </h2>
+        </div>
+    )
+}
+
+export default NotFound;
