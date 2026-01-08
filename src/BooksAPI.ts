5eafd8b9e66fe5e80a4525e17db15d5f923d37eb
xische_dhashmawy <xische_dhashmawy@dm.gov.ae>
2026-01-08 14:28:32 +0400

    fix the react api point

--- src/BooksAPI.ts
+++ src/BooksAPI.ts
@@ -1,7 +1,7 @@
 import { IBook } from "./models";
 
-const REACT_APP_API_ENDPOINT = process.env.REACT_APP_BOOKS_API_URL;
-console.log(REACT_APP_API_ENDPOINT)
+const REACT_APP_API_ENDPOINT = "https://reactnd-books-api.udacity.com";
+
 let token = localStorage.token;
 
 if (!token) token = localStorage.token = Math.random().toString(36).substr(-8);
@@ -22,7 +22,6 @@ export const getAll = () =>
     .then((data) => data.books);
 
 export const update = (book: IBook, shelf: string) =>
-
   fetch(`${REACT_APP_API_ENDPOINT}/books/${book.id}`, {
     method: "PUT",
     headers: {
@@ -30,7 +29,9 @@ export const update = (book: IBook, shelf: string) =>
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ shelf }),
-  }).then((res) => res.json()).then(data => console.log(data));
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
 
 export const search = (query: string, maxResults: number) =>
   fetch(`${REACT_APP_API_ENDPOINT}/search`, {
